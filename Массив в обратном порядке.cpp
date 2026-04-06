66522ffbcc27f07e65addff6e871d0007082122f
nasonov_890 <nasonov_890@mail.ru>
2026-04-07 00:52:15 +0300

    Massiv

---
+++ Массив в обратном порядке.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+#include <cstring>
+void PrintInfo(const char* str) {
+	int lenght = std::strlen(str);
+	std::cout << "Вывод строки на экран-";
+	for (int i = 0; i < str[i]; i++) {
+		std::cout << str[i];
+	}
+	std::cout << "\nВывод строки в обратном порядке-";
+	for (int i = lenght - 1; i >= 0; --i) {
+		std::cout << str[i] ;
+	}
+}
+
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	char string[] = { "Hello World" };
+	
+	PrintInfo(string);
+}
